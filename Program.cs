7b80211011cf69f56f92cdfbb05d972834b560f6
ganizodda <ghanievmustafo@gmail.com>
2026-06-22 12:32:07 +0500

    C#

---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿//Task1
+// List<int> numbers = new List<int> { 10, 20, 30, 40, 20, 50 };
+// int a = 20;
+// int lastindex = numbers.LastIndexOf(a);
+// Console.WriteLine("Индекси вуруди охирин: " + lastindex);
+
+
+//Task2
+// List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+// List<int> lastList = new List<int> { 2, 4 };
+// bool h = true;
+// foreach (int items in lastList) {
+//     if (!list.Contains(items)) {
+//         h = false;
+//         break;
+//     }
+// }
+// Console.WriteLine(h);
+
+
+//Task3
+// List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+// numbers.Reverse();
+// foreach (int x in raqamho)
+// {
+//     Console.Write(x + " ");
+// }
+
+
+
+//Task4
+// List<int> list = new List<int>();
+// if(list.Count == 0)
+// {
+//     System.Console.WriteLine("list xolli hast");
+// }
+// else
+// {
+//     System.Console.WriteLine("list raqam dorad");
+// }
+
+
+
+//Task5
+// List<int> getting = new List<int>(){1,2,3,4,4};
+// getting.GetRange(0,1);
+
+
+
+//Task6
+// List<int> z = new List<int>();
+// z.Sort();
+
+
+
+//Task7
+// List<int> list = new List<int>();
+// list.Clear();
+
+
+
+//Task8
+// List<int> list = new List<int>();
+// list.RemoveAll(x => x % 2 == 0);
+
+
